d8f25e54203d7a12e848b76ae10a9b8dc031ac21
Javier Suarez <luisjavier.suarezmeza@gmail.com>
2016-09-07 10:24:21 -0500

    update main.html and main.js

--- client/main.js
+++ client/main.js
@@ -3,20 +3,31 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.suma.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.suma.helpers({
+
   counter() {
     return Template.instance().counter.get();
   },
+
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
+Template.suma.events({
+
+  'click .suma'(event, instance) {
     // increment the counter when button is clicked
+    console.log("click suma");
     instance.counter.set(instance.counter.get() + 1);
   },
+  'click .resta'(event, instance) {
+    // increment the counter when button is clicked
+    console.log("click resta");
+    instance.counter.set(instance.counter.get() - 1);
+  }
+
 });
+
